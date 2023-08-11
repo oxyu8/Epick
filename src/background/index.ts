21f0aebf751e4812f72c1c8d527b294690660660
oxyu8 <liberalist0449@gmail.com>
2023-08-11 21:56:11 +0900

    add: background.ts gets a message from content.ts

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,1 +1,6 @@
-console.log('background loaded');
+console.log('background');
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.selectedText) {
+    console.log(message.selectedText);
+  }
+});
--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,11 @@
-console.log('content loaded');
+document.addEventListener('mouseup', () => {
+  const selection = window.getSelection();
+  if (selection === undefined || selection === null) {
+    return;
+  }
+  if (selection.toString().length > 0) {
+    const selectedText = selection.toString();
+    console.log(selectedText);
+    chrome.runtime.sendMessage({ selectedText });
+  }
+});
